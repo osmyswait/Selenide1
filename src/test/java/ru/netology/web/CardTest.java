a83e476d0a843a9ab8a0e50069cc3f6916734c43
Vladimir Semenov <osmyswait@gmail.com>
2023-02-18 17:51:08 +0300

    corrected  mistakes in test

--- src/test/java/ru/netology/web/CardTest.java
+++ src/test/java/ru/netology/web/CardTest.java
@@ -6,17 +6,24 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardTest {
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
     @Test
     void test() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
 
         $("[data-test-id='city'] input").setValue("Чебоксары");
+        String planningDate = generateDate(4);
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(planningDate);
         $("[data-test-id='date'] input").setValue("15.03.2023");
         $("[data-test-id='name'] input").setValue("Владимир Семенов");
         $("[data-test-id='phone'] input").setValue("+79383893838");
@@ -24,7 +31,7 @@ public class CardTest {
         $("button.button").click();
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на 15.03.2023"));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
 
